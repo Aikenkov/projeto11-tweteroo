42baefdc31f808dd8c811f70c862d7b3e157de25
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-08-26 18:48:45 -0300

    Post de usuário

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 import express from "express"
 
 const app = express();
+app.use(express.json());
+
+
+const users = [];
+
+/* = {
+    username: 'bobesponja',
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+} */
+
+let tweets = [
+    {
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    }
+]
+
+app.post("/sign-up", (req, res) => {
+    users.push(req.body)
+    res.send("Ok")
+})
+
 
 app.listen(5000, () => console.log("Escutando na porta 5000"))
