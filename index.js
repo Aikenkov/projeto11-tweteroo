83145e7580ef34a57d4e50eff4334faef118c5f8
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-08-26 19:06:57 -0300

    Post de tweets

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const users = [];
     avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
 } */
 
-let tweets = [
-    {
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    }
-]
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     users.push(req.body)
+    res.send("Ok")
+})
+
+app.post("/tweets", (req, res) => {
+    tweets.push(req.body)
     res.send("Ok")
 })
 
