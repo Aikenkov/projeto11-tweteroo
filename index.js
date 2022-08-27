b6d4836061fc0c1fec1ead65e57886cbc0d8d7da
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-08-26 22:01:04 -0300

    Retornar últimos 10 tweets

--- index.js
+++ index.js
@@ -3,49 +3,51 @@ import express from "express"
 const app = express();
 app.use(express.json());
 
-
 const users = [];
-
-/* = {
-    username: 'bobesponja',
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-    {
-  "username": "bobesponja",
-  "tweet": "eu amo o hub"
-}
-} */
-
 const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
-
     if (!username || !avatar) {
         return res.status(400).send({ erro: "Envie todos os campos!" })
     }
-
     users.push({
         username,
         avatar
     })
-
+    res.send(users)
     res.send("Ok")
 })
 
 app.post("/tweets", (req, res) => {
     const { username, tweet } = req.body;
-
     if (!username || !tweet) {
         return res.status(400).send({ erro: "Envie todos os campos!" })
     }
-
     tweets.push({
         username,
         tweet
     });
-
+    res.send(tweets)
     res.send("Ok")
 })
 
+
+app.get("/tweets", (req, res) => {
+    const lastTweets = [];
+    if (tweets.length > 0) {
+        for (let i = tweets.length - 1; i >= (tweets.length - 10) && i > 0; i--) {
+            const avatar = users.find(
+                (e) => e.username === tweets[i].username
+            ).avatar
+
+            lastTweets.push({
+                avatar,
+                ...tweets[i]
+            });
+        }
+    }
+    res.send(lastTweets)
+})
 
 app.listen(5000, () => console.log("Escutando na porta 5000"))
